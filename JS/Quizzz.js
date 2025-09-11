e9e91f85b955e9fccff5495f2f24a9cff4ee75cd
Muryllo S. Teixeira <162444926+Muryllost@users.noreply.github.com>
2025-09-11 10:53:05 -0300

    Add files via upload

---
+++ JS/Quizzz.js
@@ -0,0 +1,155 @@
+document.addEventListener("DOMContentLoaded", loadQuestion);
+
+const questao = document.querySelector("#questao");
+const opcoes = document.querySelector("#opcoes");
+const proxima = document.querySelector("#proximo");
+const mensagem = document.querySelector("#mensagem");
+const contador = document.querySelector("#contador");
+const acertosSpan = document.querySelector("#acertos");
+const reiniciar = document.querySelector("#reiniciar");
+
+let acertos = 0;
+let contadorQuestoes = 0;
+const limiteQuestoes = 10;
+let questoesAnteriores = []; // guarda os IDs das questões já usadas
+let questaoAtualGlobal = null; // guarda a questão atual exibida
+let idUser = localStorage.getItem("id_user") || 0;
+let respostasUsuario = []; // armazena todas as respostas até o final
+
+// Função para não repetir questões
+function naoRepetirQuestoes(questaoAtual, questoesAnteriores) {
+  return !questoesAnteriores.includes(questaoAtual.id_quest);
+}
+
+// Função para carregar a próxima questão
+async function loadQuestion() {
+  if (contadorQuestoes >= limiteQuestoes) {
+    try {
+      const resposta = await fetch("http://localhost:3000/perguntas/correcao", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id_user: idUser,
+          respostas: respostasUsuario,
+        }),
+      });
+
+      const resultado = await resposta.json();
+
+      proxima.disabled = true;
+      proxima.style.display = "none";
+      opcoes.style.display = "none";
+      contador.style.display = "none";
+      questao.style.display = "none";
+
+      const comemorar = document.createElement("img");
+      comemorar.src = "../../imagens/comemoracao.gif";
+      comemorar.alt = "Imagem de comemoração";
+      comemorar.style.display = "block";
+      comemorar.style.margin = "0 auto";
+      comemorar.style.width = "300px";
+      comemorar.style.height = "auto";
+
+      mensagem.innerHTML = `
+        <div style="text-align: center;">
+          <strong>Quiz finalizado! Você acertou ${resultado.acertosTentativa} de ${limiteQuestoes}.</strong>
+        </div>
+      `;
+      mensagem.appendChild(comemorar);
+      console.log(resultado);
+
+      reiniciar.disabled = false;
+      reiniciar.style.display = "block";
+      return;
+    } catch (error) {
+      console.error("Erro ao enviar respostas finais:", error);
+      mensagem.innerHTML =
+        "<p style='color:red;'>Erro ao enviar respostas para o servidor.</p>";
+      return;
+    }
+  }
+
+  try {
+    reiniciar.style.display = "none";
+    const resposta = await fetch("http://localhost:3000/perguntas");
+    const questaoAtual = await resposta.json();
+
+    // escolher uma questão aleatória que ainda não foi usada
+    let questoesValidas = questaoAtual.filter((q) =>
+      naoRepetirQuestoes(q, questoesAnteriores)
+    );
+
+    // escolhe uma questão aleatória dentre as válidas
+    const questoes =
+      questoesValidas[Math.floor(Math.random() * questoesValidas.length)];
+
+    // salvar a questão atual globalmente
+    questaoAtualGlobal = questoes;
+
+    // salvar o ID da questão já usada
+    questoesAnteriores.push(questoes.id_quest);
+
+    questao.innerText = questoes.enunciado;
+    contadorQuestoes++;
+    contador.innerText = `Questão ${contadorQuestoes} de ${limiteQuestoes}`;
+
+    opcoes.innerHTML = `
+      <label><input type="radio" name="resposta" value="alt_a">A) ${questoes.alt_a}</label><br>
+      <label><input type="radio" name="resposta" value="alt_b">B) ${questoes.alt_b}</label><br>
+      <label><input type="radio" name="resposta" value="alt_c">C) ${questoes.alt_c}</label><br>
+      <label><input type="radio" name="resposta" value="alt_d">D) ${questoes.alt_d}</label><br>
+      <label><input type="radio" name="resposta" value="alt_e">E) ${questoes.alt_e}</label><br>
+    `;
+
+    mensagem.innerHTML = "";
+  } catch (error) {
+    console.error("Erro ao carregar a questão:", error);
+    questao.innerText = "Erro ao carregar a questão.";
+  }
+}
+
+// Função para verificar a resposta selecionada e armazenar no array
+function verificarResposta() {
+  const opcoesMarcadas = document.querySelector(
+    'input[name="resposta"]:checked'
+  );
+
+  if (!opcoesMarcadas) {
+    mensagem.innerHTML =
+      '<span style="color: red;">Selecione uma opção antes de continuar.</span>';
+    return false;
+  }
+
+  const respostaSelecionada = opcoesMarcadas.value.split("_")[1]; // extrai apenas a letra da resposta
+  console.log(respostaSelecionada);
+
+  // Armazena no array de respostas do usuário
+  respostasUsuario.push({
+    id_quest: questaoAtualGlobal.id_quest,
+    resposta: respostaSelecionada,
+  });
+
+  return true;
+}
+
+// Botão de próxima questão
+proxima.addEventListener("click", async (event) => {
+  event.preventDefault();
+  const Respondida = verificarResposta();
+  if (!Respondida) {
+    return;
+  }
+  loadQuestion();
+});
+
+// Botão de reiniciar
+reiniciar.addEventListener("click", (event) => {
+  window.location.reload();
+  event.preventDefault();
+  acertos = 0;
+  contadorQuestoes = 0;
+  questoesAnteriores = [];
+  respostasUsuario = [];
+});
+
+//voltar qe
---
+++ JS/admin.js
@@ -0,0 +1,206 @@
+document.addEventListener("DOMContentLoaded", loadQuestions);
+
+const botao = document.querySelector("#cadastrar");
+botao.addEventListener("click", async function (event) {
+  event.preventDefault();
+
+  const enunciado = document.querySelector("#enunciado").value;
+  const alternativa_a = document.querySelector("#alternativa_a").value;
+  const alternativa_b = document.querySelector("#alternativa_b").value;
+  const alternativa_c = document.querySelector("#alternativa_c").value;
+  const alternativa_d = document.querySelector("#alternativa_d").value;
+  const alternativa_e = document.querySelector("#alternativa_e").value;
+  const correta = document.querySelector("#Correta").value;
+
+  //* Verificação de campo
+  if (
+    !enunciado ||
+    !alternativa_a ||
+    !alternativa_b ||
+    !alternativa_c ||
+    !alternativa_d ||
+    !alternativa_e ||
+    !correta
+  ) {
+    alert("Todos os campos são obrigatórios");
+    return;
+  }
+
+  const res = await fetch("http://localhost:3000/perguntas", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      enunciado: enunciado,
+      alt_a: alternativa_a,
+      alt_b: alternativa_b,
+      alt_c: alternativa_c,
+      alt_d: alternativa_d,
+      alt_e: alternativa_e,
+      correta: correta,
+    }),
+  });
+
+  if (res.status === 201) {
+    alert("Questão adicionada com sucesso");
+    loadQuestions();
+    document.querySelector("form").reset();
+  } else if (res.status === 409) {
+    alert("A questão já existe");
+    loadQuestions();
+  } else if (res.status === 500) {
+    alert("Erro inesperado");
+  } else {
+    console.error();
+  }
+});
+
+//*Carregar questões
+async function loadQuestions() {
+  const questionList = document.getElementById("questionList");
+  questionList.innerHTML = "";
+
+  try {
+    const response = await fetch("http://localhost:3000/perguntas/cards");
+    const questions = await response.json();
+
+    questions.forEach((questoes) => {
+      addQuestionToPage(questoes);
+    });
+  } catch (error) {
+    console.error("Erro ao carregar perguntas:", error);
+  }
+}
+
+//* Cards
+async function addQuestionToPage(questoes) {
+  const questionList = document.getElementById("questionList");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const hiddenIdInput = document.createElement("input");
+  hiddenIdInput.type = "hidden";
+  hiddenIdInput.value = questoes.id_quest;
+  hiddenIdInput.classList.add("question-id");
+
+  const questionTitle = document.createElement("h3");
+  questionTitle.classList.add("card-title");
+  questionTitle.innerText = `Pergunta: ${questoes.enunciado}`;
+
+  const alternatives = document.createElement("ul");
+  alternatives.classList.add("card-alternatives");
+  alternatives.innerText = `A: ${questoes.alt_a}
+B: ${questoes.alt_b}
+C: ${questoes.alt_c}
+D: ${questoes.alt_d}
+E: ${questoes.alt_e}`;
+
+  let textoCorreto = "";
+  if (questoes.correta === "alternativa_a") textoCorreto = questoes.alt_a;
+  else if (questoes.correta === "alternativa_b") textoCorreto = questoes.alt_b;
+  else if (questoes.correta === "alternativa_c") textoCorreto = questoes.alt_c;
+  else if (questoes.correta === "alternativa_d") textoCorreto = questoes.alt_d;
+  else if (questoes.correta === "alternativa_e") textoCorreto = questoes.alt_e;
+  else textoCorreto = "Alternativa inválida";
+  //* Questão correta
+  const correctAnswer = document.createElement("p");
+  correctAnswer.classList.add("card-correct-answer");
+  correctAnswer.innerText = `Resposta correta: ${textoCorreto}`;
+
+  //*Botao de delete
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Excluir";
+  deleteButton.classList.add("delete-button");
+  deleteButton.addEventListener("click", async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/perguntas/${questoes.id_quest}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        alert("Pergunta excluída");
+        card.remove();
+      } else {
+        alert("Erro ao deletar a pergunta.");
+      }
+    } catch (error) {
+      console.error("Erro na exclusão:", error);
+    }
+  });
+
+  //* Botão de editar
+  const editbutton = document.createElement("button");
+  editbutton.innerText = "Editar";
+  editbutton.classList.add("edit-button");
+
+  editbutton.addEventListener("click", () => {
+    const modal = document.getElementById("modal-editar");
+
+    document.getElementById("edit-enunciado").value = questoes.enunciado;
+    document.getElementById("edit-a").value = questoes.alt_a;
+    document.getElementById("edit-b").value = questoes.alt_b;
+    document.getElementById("edit-c").value = questoes.alt_c;
+    document.getElementById("edit-d").value = questoes.alt_d;
+    document.getElementById("edit-e").value = questoes.alt_e;
+    document.getElementById("edit-correta").value = questoes.correta;
+
+    document
+      .getElementById("salvar-edicao")
+      .setAttribute("data-id", questoes.id_quest);
+
+    modal.showModal();
+  });
+  //* Mostrar Cards
+  card.append(
+    questionTitle,
+    hiddenIdInput,
+    alternatives,
+    correctAnswer,
+    deleteButton,
+    editbutton
+  );
+  questionList.appendChild(card);
+}
+//* Modal
+document
+  .getElementById("salvar-edicao")
+  .addEventListener("click", async (event) => {
+    const id_quest = event.target.getAttribute("data-id");
+
+    const atualizado = {
+      newEnunciado: document.getElementById("edit-enunciado").value.trim(),
+      alt_a: document.getElementById("edit-a").value.trim(),
+      alt_b: document.getElementById("edit-b").value.trim(),
+      alt_c: document.getElementById("edit-c").value.trim(),
+      alt_d: document.getElementById("edit-d").value.trim(),
+      alt_e: document.getElementById("edit-e").value.trim(),
+      correta: document.getElementById("edit-correta").value,
+    };
+    try {
+      const response = await fetch(
+        `http://localhost:3000/perguntas/${id_quest}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(atualizado),
+        }
+      );
+      if (response.ok) {
+        alert("Editado com sucesso!");
+        loadQuestions();
+      } else {
+        alert("Erro");
+      }
+    } catch (error) {
+      console.log("Erro ao Editar", error);
+    }
+
+    document.getElementById("modal-editar").close();
+    loadQuestions();
+  });
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+  document.getElementById("modal-editar").close();
+});
